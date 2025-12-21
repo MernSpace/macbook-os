7f3ed85de0212e5028e7b6388e31ef69fc53502e
mernspace <sifat355y@gmail.com>
2025-12-21 19:06:19 +0600

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "#components/NavBar"
+import Welcome from "#components/Welcome"
 
 const App =()=>{
   return(
     <main>
       <NavBar/>
+      <Welcome/>
     </main>
   )
 }
